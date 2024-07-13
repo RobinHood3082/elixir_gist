ecd9169e510180a17487760a1cc8b394b590e5d1
RobinHood3082 <rkhan3082@gmail.com>
2024-07-13 17:45:49 +0600

    add: navbar

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -14,7 +14,28 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand: "#FD4F00",
+        egDark: {
+          dark: "#010101",
+          DEFAULT: "#3C3B3B",
+          light: "#828282"
+        },
+        egPurple: {
+           dark: "#631A61",
+           DEFAULT: "#A834A5",
+           light: "#E88Fe5"
+        },
+        egLavender: {
+          dark: "#8C52FF",
+          DEFAULT: "#9D74EE",
+          light: "#BC9AFF"
+        }
+      },
+      fontFamily: {
+        brand: ["WorkSans", "sans-serif"]
+      },
+      fontWeight: {
+        regular: "normal",
+        bold: "bold"
       }
     },
   },
